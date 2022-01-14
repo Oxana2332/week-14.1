204b066e3b5da4f70f390f31630dea3dbaeda247
Oxana2332 <far_north@ro.ru>
2022-01-14 11:58:16 +0300

    strange gallery

---
+++ gallery.js
@@ -0,0 +1,9 @@
+function back() {
+    let image1 = document.getElementById('img');
+    image1.src = "./assets/image/city.jpg";
+}
+
+function forward() {
+    let image1 = document.getElementById('img');
+    image1.src = "./assets/image/lighthouse.jpg";
+}
